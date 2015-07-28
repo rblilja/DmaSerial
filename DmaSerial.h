6591911de7abebe1afe3af4c3f5367492d7e6cdc
Robin Lilja <robin.lilja@gmail.com>
2015-07-28 23:21:27 +0200

    Update DmaSerial.h

--- DmaSerial.h
+++ DmaSerial.h
@@ -2,7 +2,7 @@
  * "THE BEER-WARE LICENSE" (Revision 42):
  * <robin.lilja@gmail.com> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return Robin Lilja
+ * this stuff is worth it, you can buy me a beer in return. - Robin Lilja
  *
  * @see page 519 in ATMEL SAM3X/A datasheet/manual for Peripheral DMA Controller (PDC) usage.  
  *
